4fc6f020ed0874f054589b23e2e900e9207f9026
EdGeber <egma@cin.ufpe.br>
2022-04-08 21:28:32 -0300

    Setup of the component structure of fv-gui

--- fv-gui/src/app/app.module.ts
+++ fv-gui/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import here the root component of each page. For example,
+// TopBarComponent is the root component of main-page. We
+// know that because the name of its selector ends in "root"
+// (it is 'main-page-root')
+import { TopBarComponent } from './main-page/top-bar/top-bar.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        TopBarComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+            // add here the path of each root component imported above.
+            // The path name is arbitrary, you give it the name you want,
+            // but it should be the name of the url of the page. For
+            // example, TopBarComponent is the root of the main page,
+            // so its path name is 'main'.
+            {
+                path: 'main',
+                component: TopBarComponent
+            },
+        ])
+    ],
+    // don't change
+    providers: [],
+    bootstrap: [TopBarComponent]
 })
 export class AppModule { }
--- fv-gui/src/app/main-page/top-bar/top-bar.component.spec.ts
+++ fv-gui/src/app/main-page/top-bar/top-bar.component.spec.ts
@@ -1,33 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar.component';
 
-describe('AppComponent', () => {
+describe('TopBarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        TopBarComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(TopBarComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'fv-gui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(TopBarComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('fv-gui');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(TopBarComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('fv-gui app is running!');
