5c68e336f7f8b70e9b388ca3947d13bbbadf4dee
EdGeber <egma@cin.ufpe.br>
2022-05-06 19:46:06 -0300

    Add server configurations

--- fv-server/main.ts
+++ fv-server/main.ts
@@ -1,12 +1,22 @@
 import express from "express";
 
 const fvServer = express();
-const port = 3000;
+const PORT = 3000;
+
+var allowCrossDomain = function(req: any, res: any, next: any) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+fvServer.use(allowCrossDomain);
+
+fvServer.use(express.json());
 
 fvServer.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-fvServer.listen(port, () => {
-  console.log(`Forumverse Server listening on port ${port}!`)
+fvServer.listen(PORT, () => {
+  console.log(`Forumverse Server listening on port ${PORT}!`)
 })
