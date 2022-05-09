9a429962b8dc2b3cb1e80c7f5db4ecbae6ff92f8
AmadeoNeto <amadeotatocota@gmail.com>
2022-05-09 00:35:10 -0300

    Fixed IDs bug

--- common/Reply.ts
+++ common/Reply.ts
@@ -1,17 +1,13 @@
 import { User } from "./User";
 
-export class Reply{
-    static totalReplies = 0;
-    
-    id: number;
+export class Reply{    
+    public id: number = -1;
     author: User;
     timeSent: Date;
     content: string;
     quoteOf: Reply|null;
 
     constructor(author:User, content:string, quotedReply:Reply|null = null){
-        Reply.totalReplies++;
-        this.id = Reply.totalReplies;
         this.author = author;
         this.content = content;
         this.timeSent = new Date();
